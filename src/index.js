efe24293136feefd48a181ec8dbc8258fba3aba3
Uladzislau <Uladzislau_Selikh@epam.com>
2021-01-05 22:29:26 +0300

    fix: delete redundant variable

--- src/index.js
+++ src/index.js
@@ -1,40 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  if (str.length % 2 === 1)
-  return false;
+
   let arrOpen = [];
   let arrClose = [];
-  let stack = [];
+  let stack = []; //variable to store open brackets
+
   for (let i = 0; i < bracketsConfig.length; i++) {
-        arrOpen.push(bracketsConfig[i][0]);
-        arrClose.push(bracketsConfig[i][1]);
-      }
-      let counter = [];
-      let current;
+    arrOpen.push(bracketsConfig[i][0]);
+    arrClose.push(bracketsConfig[i][1]);
+  }
+
+  let current;
+
   for (let m = 0; m < str.length; m++) {
-        current = str[m]; //easier than writing it over and over
-        if (arrOpen.indexOf(current) > -1 && arrOpen.indexOf(current) === arrClose.indexOf(current) && counter.indexOf(current) === -1) {
-          stack.push(current);
-          counter.push(current);
-        } else if (arrOpen.indexOf(current) > -1 && arrOpen.indexOf(current) === arrClose.indexOf(current) && counter.indexOf(current) !== -1) {
-          const LastBracket = arrOpen.indexOf(stack.pop());
-          counter.pop();
-          if (arrClose[LastBracket] !== current) { //if the stack is empty, .pop() returns undefined, so this expression is still correct
-          
-            return false; //terminate immediately - no need to continue scanning the string
-          }
-        }
-          else if (arrOpen.indexOf(current) > -1 && arrOpen.indexOf(current) !== arrClose.indexOf(current)) {
-            stack.push(current);
-        }
-        else {
-          const LastBracket = arrOpen.indexOf(stack.pop());
-          
-          if (arrClose[LastBracket] !== current) { //if the stack is empty, .pop() returns undefined, so this expression is still correct
-          
-            return false; //terminate immediately - no need to continue scanning the string
-          }
-        }
-      }
+
+    current = str[m]; //easier than writing it over and over
+
+    if (arrOpen.indexOf(current) > -1 && arrOpen.indexOf(current) === arrClose.indexOf(current) && stack.indexOf(current) === -1) {
       
-      return stack.length === 0; //any elements mean brackets left open
+      stack.push(current);
+
+    } else if (arrOpen.indexOf(current) > -1 && arrOpen.indexOf(current) === arrClose.indexOf(current) && stack.indexOf(current) !== -1) {
+
+      const LastBracket = arrOpen.indexOf(stack.pop());
+
+      if (arrClose[LastBracket] !== current) { //if the stack is empty, .pop() returns undefined, so this expression is still correct
+
+        return false; //if the last open bracket added to the stack doesn't correspont to the current closing bracket, configuration is incorrect
+      }
+    } else if (arrOpen.indexOf(current) > -1 && arrOpen.indexOf(current) !== arrClose.indexOf(current)) {
+      
+      stack.push(current);
+
+    } else {
+
+      const LastBracket = arrOpen.indexOf(stack.pop());
+
+      if (arrClose[LastBracket] !== current) { //if the stack is empty, .pop() returns undefined, so this expression is still correct
+
+        return false; //see line 27
+      }
+    }
+  }
+
+  return stack.length === 0; //any elements mean brackets left open
 }
